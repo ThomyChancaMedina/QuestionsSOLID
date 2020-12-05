4559ce53505c8624b9e4d4567480fd9c03038baf
thomychancamedina <(Rocky20!*)>
2020-12-05 20:18:36 +0100

    Setup Github Actions

--- app/src/main/java/com/architectcoders/grupo2verano2020/NavHostActivity.kt
+++ app/src/main/java/com/architectcoders/grupo2verano2020/NavHostActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 class NavHostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContentView(R.layout.activity_nav_host)
     }
 }
